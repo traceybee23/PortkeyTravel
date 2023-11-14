d737b1ed8e00a88799569e0533ad1317af461edc
traceybee23 <tracebee23@gmail.com>
2023-11-13 18:24:27 -0600

    phase 3 authenticateMe complete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,34 @@
 const router = require('express').Router();
+const { restoreUser, requireAuth, setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
+router.use(restoreUser);
 
-router.post('/test', function(req, res) {
+router.post('/test', function (req, res) {
     res.json({ requestBody: req.body })
-})
+});
 
+
+// // GET /api/set-token-cookie
+// router.get('/set-token-cookie', async (_req, res) => {
+//     const user = await User.findOne({
+//         where: {
+//             username: 'Demo-lition'
+//         }
+//     });
+//     setTokenCookie(res, user);
+//     return res.json({ user: user });
+// });
+
+// // GET /api/restore-user
+// router.get('/restore-user', (req, res) => {
+//     return res.json(req.user);
+// });
+
+
+// // GET /api/require-auth
+// router.get('/require-auth', requireAuth, (req, res) => {
+//     return res.json(req.user);
+// });
 
 module.exports = router;
