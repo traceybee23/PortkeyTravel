f6e06aa6ad437c2f31438b390599d43971c13b90
traceybee23 <tracebee23@gmail.com>
2023-11-28 18:36:21 -0600

    demo images seed data done

--- backend/db/seeders/20231129000856-demo-image.js
+++ backend/db/seeders/20231129000856-demo-image.js
@@ -11,45 +11,35 @@ const demoImages = [
   {
     "imageableId": "1",
     "imageableType": "Review",
-    "url": "fkjd",
+    "url": "https://images.unsplash.com/photo-1426122402199-be02db90eb90?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c2FuJTIwZnJhbmNpc2NvJTIwaG91c2V8ZW58MHx8MHx8fDA%3D",
   },
   {
     "imageableId": "2",
     "imageableType": "Spot",
-    "url": "fkjd",
+    "url": "https://images.unsplash.com/photo-1633511090164-b43840ea1607?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8aGF3YWlpJTIwaG91c2V8ZW58MHx8MHx8fDA%3D",
   },
   {
     "imageableId": "3",
     "imageableType": "Review",
-    "url": "fkjd",
+    "url": "https://images.unsplash.com/photo-1608463123864-40a2961b7d00?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDJ8fGhvdXNlJTIwaW4lMjBzZWF0dGxlfGVufDB8fDB8fHww",
   },
   {
     "imageableId": "4",
     "imageableType": "Spot",
-    "url": "fkjd",
+    "url": "https://images.unsplash.com/photo-1615874959474-d609969a20ed?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aG91c2UlMjBpbiUyMGF1c3RpbiUyMFRYfGVufDB8fDB8fHww",
   },
 ]
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await Spot.bulkCreate(demoImages, { validate: true })
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    options.tableName = 'Images';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options,
+      { [Op.or]: demoImages }, {});
   }
 };
