2807513fefa659c66a790f2535ecf3e0382b1df1
traceybee23 <tracebee23@gmail.com>
2023-11-30 17:37:24 -0600

    finished Get all Spots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -5,32 +5,60 @@ const { handleValidationErrors } = require('../../utils/validation')
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { Spot, Review, Image } = require('../../db/models');
+const e = require('express');
 
 const router = express.Router();
 
 
 router.get('/', async (req, res) => {
 
-const spots = await Spot.findAll({
-    include: [{
-        model: Review,
-        attributes: {
-            exclude: ['userId', 'spotId', 'review', 'createdAt', 'updatedAt']
-        }
-    }],
-    // include: [{
-    //     model: Image,
-    //     attributes: {
-    //         exclude: ['id', 'imageableId', 'imageableType', 'createdAt', 'updatedAt']
-    //     }
-    // }]
-})
+    const spots = await Spot.findAll({
+        include: [
+            {
+                model: Review,
+                attributes: ['stars'],
+            },
+            {
+                model: Image,
+                attributes: ['url'],
+            }
+        ],
+    });
 
+    let spotsList = [];
 
+    spots.forEach(spot => {
+        //console.log(spot)
+        spotsList.push(spot.toJSON())
+    })
 
+    ///get avgRating
+    let stars = [];
+    spotsList.forEach(spot => {
+        spot.Reviews.forEach(review => {
+            if (spot.Reviews.length > 1) {
+                stars.push(review.stars)
+                spot.avgRating = (stars.reduce((acc, curr) => acc + curr, 0) / stars.length)
+            } else {
+                spot.avgRating = review.stars
+            }
+        })
+        delete spot.Reviews
+    })
 
-res.json({ "Spots": spots})
-
+    //attach images
+    spotsList.forEach(spot => {
+        spot.Images.forEach(image => {
+            //console.log(image.url)
+            if (!spot.Images) {
+                spot.previewImage = "image url"
+            } else {
+                spot.previewImage = image.url
+            }
+        })
+        delete spot.Images
+    })
+    res.json({ Spots: spotsList })
 })
 
 module.exports = router;
