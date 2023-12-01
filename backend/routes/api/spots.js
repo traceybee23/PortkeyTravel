5ed673f84535d4a595bd1110bbe670f538ee46fc
traceybee23 <tracebee23@gmail.com>
2023-11-30 18:10:58 -0600

    get all spots owned by current user

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -9,6 +9,66 @@ const e = require('express');
 
 const router = express.Router();
 
+router.get('/current', requireAuth, async (req, res, next) => {
+
+    const { user } = req;
+
+    if (user) {
+        const safeUser = {
+            ownerId: user.id
+        }
+        console.log(safeUser)
+        const spots = await Spot.findAll({
+            where: safeUser,
+            include: [
+                {
+                    model: Review,
+                    attributes: ['stars'],
+                },
+                {
+                    model: Image,
+                    attributes: ['url'],
+                }
+            ],
+        })
+        let spotsList = [];
+
+        spots.forEach(spot => {
+            //console.log(spot)
+            spotsList.push(spot.toJSON())
+        })
+
+        ///get avgRating
+        let stars = [];
+        spotsList.forEach(spot => {
+            spot.Reviews.forEach(review => {
+                if (spot.Reviews.length > 1) {
+                    stars.push(review.stars)
+                    spot.avgRating = (stars.reduce((acc, curr) => acc + curr, 0) / stars.length)
+                } else {
+                    spot.avgRating = review.stars
+                }
+            })
+            delete spot.Reviews
+        })
+
+        //attach images
+        spotsList.forEach(spot => {
+            spot.Images.forEach(image => {
+                //console.log(image.url)
+                if (!spot.Images) {
+                    spot.previewImage = "image url"
+                } else {
+                    spot.previewImage = image.url
+                }
+            })
+            delete spot.Images
+        })
+        res.json({ Spots: spotsList })
+    }
+
+})
+
 
 router.get('/', async (req, res) => {
 
