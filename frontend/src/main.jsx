2b7216c34345abeb26ae2d8a4db0a3433a7daf16
traceybee23 <tracebee23@gmail.com>
2024-01-11 13:43:49 -0600

    added store to window

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,10 @@ import configureStore from './store';
 
 const store = configureStore();
 
+if (import.meta.env.MODE !== "production") {
+  window.store = store;
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
