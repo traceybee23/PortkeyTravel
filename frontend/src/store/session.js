c0eb9a954454cceed17583631f9c4802dd9fdb99
traceybee23 <tracebee23@gmail.com>
2024-01-11 19:51:43 -0600

    sign up action really complete this time

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -33,7 +33,7 @@ export const login = (user) => async (dispatch) => {
   return response
 }
 
-export const restoreUser = (user) => async (dispatch) => {
+export const restoreUser = () => async (dispatch) => {
   const response = await csrfFetch("/api/session")
   const data = await response.json();
   dispatch(setUser(data.user));
