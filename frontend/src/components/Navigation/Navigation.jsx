275eadcb9207b4fe48dd992fe9a5128317a11e9d
traceybee23 <tracebee23@gmail.com>
2024-01-12 13:08:45 -0600

    Modal login complete phase 4 auth me

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,31 +1,28 @@
 import { NavLink } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
-import * as sessionActions from '../../store/session'
+import OpenModalButton from "../OpenModalButton";
+import LoginFormModal from "../LoginFormModal";
 import './Navigation.css'
 
 function Navigation({ isLoaded }) {
-  const dispatch = useDispatch();
+
   const sessionUser = useSelector(state => state.session.user);
 
-  const logout = (e) => {
-    e.preventDefault();
-    dispatch(sessionActions.logout());
-  }
 
   const sessionLinks = sessionUser ? (
     <>
       <li>
         <ProfileButton user={sessionUser} />
       </li>
-      <li>
-          <button onClick={logout}>Log Out</button>
-        </li>
     </>
   ) : (
     <>
       <li>
-        <NavLink to="/login">Log In</NavLink>
+        <OpenModalButton
+        buttonText="Log In"
+        modalComponent={<LoginFormModal />}
+        />
       </li>
       <li>
         <NavLink to="/signup">Sign Up</NavLink>
