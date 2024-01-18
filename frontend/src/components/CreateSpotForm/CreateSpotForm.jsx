23eebb4dc4c0fa6cbfd5b3ac651870c564cacb23
traceybee23 <tracebee23@gmail.com>
2024-01-17 21:14:20 -0600

    error handling create spot form

--- frontend/src/components/CreateSpotForm/CreateSpotForm.jsx
+++ frontend/src/components/CreateSpotForm/CreateSpotForm.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux"
 import { createSpot } from "../../store/spots";
-// import * as sessionActions from '../../store/session'
 import './CreateSpotForm.css'
 
 const CreateSpotForm = () => {
@@ -14,8 +13,8 @@ const CreateSpotForm = () => {
   const [address, setAddress] = useState('');
   const [city, setCity] = useState('');
   const [state, setState] = useState('');
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
+  const [lat, setLat] = useState('');
+  const [lng, setLng] = useState('');
   const [description, setDescription] = useState('');
   const [name, setName] = useState('');
   const [price, setPrice] = useState('');
@@ -24,20 +23,22 @@ const CreateSpotForm = () => {
   const [image2, setImage2] = useState('');
   const [image3, setImage3] = useState('');
   const [image4, setImage4] = useState('');
+  const [errors, setErrors] = useState({});
 
-  // useEffect(() => {
-  //   dispatch(sessionActions.restoreUser())
-  // },[dispatch])
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setErrors({})
+
+    if(!lat.length) setErrors(errors.lat ='Latitude is required')
+
     const newSpot = {
       country,
       address,
       city,
       state,
-      latitude,
-      longitude,
+      lat,
+      lng,
       description,
       name,
       price,
@@ -47,8 +48,17 @@ const CreateSpotForm = () => {
       image3,
       image4
     }
-    dispatch(createSpot(newSpot));
-    reset();
+    return dispatch(createSpot(newSpot))
+    .then(reset())
+    .catch(
+      async (res) => {
+        const data = await res.json();
+        if (data && data.errors) {
+          setErrors(data.errors)
+        }
+      }
+    )
+
   }
 
   const reset = () => {
@@ -56,8 +66,8 @@ const CreateSpotForm = () => {
     setAddress(''),
     setCity('');
     setState('');
-    setLatitude('');
-    setLongitude('');
+    setLat('');
+    setLng('');
     setDescription('');
     setName('');
     setPrice('');
@@ -83,6 +93,7 @@ const CreateSpotForm = () => {
             placeholder="Country"
             name="country"
           />
+          {errors.country && <span className="errors">{errors.country}</span>}
           <input
             type="text"
             value={address}
@@ -90,6 +101,7 @@ const CreateSpotForm = () => {
             placeholder="Address"
             name="address"
           />
+          {errors.address && <span className="errors">{errors.address}</span>}
           <input
             type="text"
             value={city}
@@ -97,6 +109,7 @@ const CreateSpotForm = () => {
             placeholder="City"
             name="city"
           />
+          {errors.city && <span className="errors">{errors.city}</span>}
           <input
             type="text"
             value={state}
@@ -104,26 +117,30 @@ const CreateSpotForm = () => {
             placeholder="State"
             name="state"
           />
+          {errors.state && <span className="errors">{errors.state}</span>}
           <input
             type="text"
-            value={latitude}
-            onChange={(e) => setLatitude(e.target.value)}
+            value={lat}
+            onChange={(e) => setLat(e.target.value)}
             placeholder="Latitude"
-            name="latitiude"
+            name="lat"
           />
+          {errors.lat && <span className="errors">{errors.lat}</span>}
           <input
             type="text"
-            value={longitude}
-            onChange={(e) => setLongitude(e.target.value)}
+            value={lng}
+            onChange={(e) => setLng(e.target.value)}
             placeholder="Longitude"
-            name="longitude"
+            name="lng"
           />
+          {errors.lng && <span className="errors">{errors.lng}</span>}
           <textarea
             value={description}
             onChange={(e) => setDescription(e.target.value)}
-            placeholder="Please write at least 30 characters"
+            placeholder="Description"
             name="description"
           />
+          {errors.description && <span className="errors">{errors.description}</span>}
           <input
             type="text"
             value={name}
@@ -131,6 +148,7 @@ const CreateSpotForm = () => {
             placeholder="Name of your spot"
             name="name"
           />
+          {errors.name && <span className="errors">{errors.name}</span>}
           <input
             type="text"
             value={price}
@@ -138,6 +156,7 @@ const CreateSpotForm = () => {
             placeholder="Price per night (USD)"
             name="price"
           />
+          {errors.price && <span className="errors">{errors.price}</span>}
           <input
             type="src"
             value={previewImage}
@@ -145,6 +164,7 @@ const CreateSpotForm = () => {
             placeholder="Preview Image URL"
             name="previewImage"
           />
+          {errors.previewImage && <span className="errors">{errors.previewImage}</span>}
           <input
             type="src"
             value={image1}
@@ -152,6 +172,7 @@ const CreateSpotForm = () => {
             placeholder="Image URL"
             name="image1"
           />
+          {errors.image && <span className="errors">{errors.image}</span>}
           <input
             type="src"
             value={image2}
@@ -159,6 +180,7 @@ const CreateSpotForm = () => {
             placeholder="Image URL"
             name="image"
           />
+          {errors.image && <span className="errors">{errors.image}</span>}
           <input
             type="src"
             value={image3}
@@ -166,6 +188,7 @@ const CreateSpotForm = () => {
             placeholder="Image URL"
             name="image"
           />
+          {errors.image && <span className="errors">{errors.image}</span>}
           <input
             type="src"
             value={image4}
@@ -173,6 +196,7 @@ const CreateSpotForm = () => {
             placeholder="Image URL"
             name="image"
           />
+          {errors.image && <span className="errors">{errors.image}</span>}
           <button type="submit">Create Spot</button>
         </form>
       }
