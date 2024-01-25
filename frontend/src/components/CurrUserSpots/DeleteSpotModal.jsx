1c58979e70aefa709164b57ffa6a1e1351b9aa53
traceybee23 <tracebee23@gmail.com>
2024-01-25 15:30:06 -0600

    added css to images

--- frontend/src/components/CurrUserSpots/DeleteSpotModal.jsx
+++ frontend/src/components/CurrUserSpots/DeleteSpotModal.jsx
@@ -2,7 +2,7 @@ import { useDispatch } from "react-redux";
 import { useModal } from '../../context/Modal';
 import { deleteSpot } from "../../store/spots";
 
-const DeleteSpot = ({spotId}) => {
+const DeleteSpot = ({ spotId }) => {
 
   const dispatch = useDispatch();
   const { closeModal } = useModal();
@@ -11,20 +11,22 @@ const DeleteSpot = ({spotId}) => {
   const handleDelete = (e) => {
     e.preventDefault();
     dispatch(deleteSpot(spotId.spotId))
-    .then(closeModal)
-    .catch(async (response) => {
-      const data = await response.json();
-      return data;
-    })
+      .then(closeModal)
+      .catch(async (response) => {
+        const data = await response.json();
+        return data;
+      })
   }
 
   return (
-    <li>
-    <h3>Confirm Delete</h3>
-    <span>Are you sure you want to delete this spot?</span>
-    <button onClick={handleDelete}>Yes (Delete Spot)</button>
-    <button style={{backgroundColor: "darkgrey"}} onClick={closeModal}>No (Keep Spot)</button>
-  </li>
+    <>
+      <div className="deleteModal">
+        <h3>Confirm Delete</h3>
+        <span>Are you sure you want to delete this spot?</span>
+        <button onClick={handleDelete}>Yes (Delete Spot)</button>
+        <button style={{ backgroundColor: "darkgrey" }} onClick={closeModal}>No (Keep Spot)</button>
+      </div>
+    </>
   )
 }
 
